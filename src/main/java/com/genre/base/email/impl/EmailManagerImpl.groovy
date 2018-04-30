429e5a32eb906a000c79940430d654c3939c621e
genreboy <javascriptjenkins@gmail.com>
2018-04-29 22:25:20 -0500

    refactor to groovy 2

--- src/main/java/com/genre/base/email/impl/EmailManagerImpl.groovy
+++ src/main/java/com/genre/base/email/impl/EmailManagerImpl.groovy
@@ -16,51 +16,51 @@ import javax.mail.internet.MimeMessage
 @Component
 class EmailManagerImpl implements EmailManager {
 
-    private static Properties mailServerProperties;
-    private static Session getMailSession;
-    private static MimeMessage generateMailMessage;
+    private static Properties mailServerProperties
+    private static Session getMailSession
+    private static MimeMessage generateMailMessage
 
     void generateAndSendEmail(String dataToSend, ArrayList<String> emailList) throws AddressException, MessagingException {
 
         // Step1
-        System.out.println("\n 1st ===> setup Mail Server Properties..");
-        mailServerProperties = System.getProperties();
-        mailServerProperties.put("mail.smtp.port", "587");
-        mailServerProperties.put("mail.smtp.auth", "true");
-        mailServerProperties.put("mail.smtp.starttls.enable", "true");
-        mailServerProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-        System.out.println("Mail Server Properties have been setup successfully..");
+        System.out.println("\n 1st ===> setup Mail Server Properties..")
+        mailServerProperties = System.getProperties()
+        mailServerProperties.put("mail.smtp.port", "587")
+        mailServerProperties.put("mail.smtp.auth", "true")
+        mailServerProperties.put("mail.smtp.starttls.enable", "true")
+        mailServerProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com")
+        System.out.println("Mail Server Properties have been setup successfully..")
 
         // Step2
-        System.out.println("\n\n 2nd ===> get Mail Session..");
-        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
-        generateMailMessage = new MimeMessage(getMailSession);
+        System.out.println("\n\n 2nd ===> get Mail Session..")
+        getMailSession = Session.getDefaultInstance(mailServerProperties, null)
+        generateMailMessage = new MimeMessage(getMailSession)
 
         for(String email: emailList){
-            generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(email))
         }
 
-        generateMailMessage.setSubject("china wok take ur order preez how many craigslist results u like rong time");
-        String emailBody = dataToSend;
-        generateMailMessage.setContent(emailBody, "text/html");
-        System.out.println("Mail Session has been created successfully..");
+        generateMailMessage.setSubject("china wok take ur order preez how many craigslist results u like rong time")
+        String emailBody = dataToSend
+        generateMailMessage.setContent(emailBody, "text/html")
+        System.out.println("Mail Session has been created successfully..")
 
         // Step3
-        System.out.println("\n\n 3rd ===> Get Session and Send mail");
-        Transport transport = getMailSession.getTransport("smtp");
+        System.out.println("\n\n 3rd ===> Get Session and Send mail")
+        Transport transport = getMailSession.getTransport("smtp")
 
         // Enter your correct gmail UserID and Password
         // if you have 2FA enabled then provide App Specific Password
-        transport.connect("smtp.gmail.com", "craigslistbabygurl@gmail.com", "scrape11");
-        transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
-        transport.close();
+        transport.connect("smtp.gmail.com", "craigslistbabygurl@gmail.com", "scrape11")
+        transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients())
+        transport.close()
     }
 
 
 
     private String formatCraigslistObjectsToEmailHTML(ArrayList<CraigslistObject> craigslistObjects){
 
-        StringBuilder emailHTML = new StringBuilder();
+        StringBuilder emailHTML = new StringBuilder()
 
         for(CraigslistObject craigslistObject: craigslistObjects){
             emailHTML.append("<h2>" + craigslistObject.getPostTitle() + "</h2>")
@@ -69,7 +69,7 @@ class EmailManagerImpl implements EmailManager {
             emailHTML.append("<hr>") // adds a line under each result
         }
 
-        return emailHTML.toString();
+        return emailHTML.toString()
     }
 
 
