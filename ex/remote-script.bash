7f8c810afdefca427197cb110f56e8a5704c0932
genreboy <javascriptjenkins@gmail.com>
2018-05-20 20:16:10 -0500

    add a remote script 10

--- ex/remote-script.bash
+++ ex/remote-script.bash
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
 
+### Assumptions:
+# 1) The docker user will be logged in on remote server where this is executing.
+#
+#
+
 echo 'OMG LOOK A REMOTE SCRIPT IS EXECUTING'
 
 docker container 'ls'
 
 docker kill sb-scrape
+echo 'WAITING 2 seconds while sb-scape is killed!'
+sleep 2s
 
-echo 'WAITING 5 seconds while sb-scape is killed!'
+docker container rm sb-scrape
+echo 'WAITING 2 seconds while sb-scape container name is removed!'
+sleep 2s
 
-sleep 5s
-
+echo 'Running new detached instance of container!'
 docker run -d --name sb-scrape javascriptjenkins/sb-scrape:latest
 
 
