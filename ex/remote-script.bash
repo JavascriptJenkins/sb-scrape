93309f75cac074ae54307ddd0692e022c20ba3be
genreboy <javascriptjenkins@gmail.com>
2018-05-20 20:19:20 -0500

    add a remote script 11

--- ex/remote-script.bash
+++ ex/remote-script.bash
@@ -18,6 +18,10 @@ docker container rm sb-scrape
 echo 'WAITING 2 seconds while sb-scape container name is removed!'
 sleep 2s
 
+docker pull javascriptjenkins/sb-scrape:latest
+echo 'WAITING 2 seconds while sb-scape :latest is pulled for new changes!'
+sleep 2s
+
 echo 'Running new detached instance of container!'
 docker run -d --name sb-scrape javascriptjenkins/sb-scrape:latest
 
