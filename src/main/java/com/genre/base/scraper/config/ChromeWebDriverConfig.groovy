a95a3b60f5cb37c4f20b50f381d566b7e5cf6595
genreboy <javascriptjenkins@gmail.com>
2019-03-24 21:28:57 -0500

    uipdarte

--- src/main/java/com/genre/base/scraper/config/ChromeWebDriverConfig.groovy
+++ src/main/java/com/genre/base/scraper/config/ChromeWebDriverConfig.groovy
@@ -15,7 +15,7 @@ class ChromeWebDriverConfig {
     boolean initChromeDriver(){
 //        System.setProperty("webdriver.gecko.driver","/Users/genreboy/Downloads/chromedriver.exe")
         logger.info("INIT CHROME DRIVER SETTINGS")
-        System.setProperty("webdriver.chrome.driver","chromedriver/chromedriver")
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver/chromedriver")
         return true
     }
 
